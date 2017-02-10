6c15b82d01d645241c4f5d8832f2795e0f9f86c9
richard-rhall <richard@r-hall.co.uk>
2017-02-10 10:38:50 +0000

    Update Revised Filepath

--- OperaVSS.ps1
+++ OperaVSS.ps1
@@ -1,13 +1,14 @@
 ﻿# OperaVSS - Richard Hall
-$strFileName="F:\richardscripts\watchdirectory\opera.txt"
+$strFileName="FULL PATH HERE"
 IF (Test-Path $strFileName){
+# msg can be removed if wanted
 msg * /SERVER:LT-0676 FILE EXISTS
 # gwmi - Starts VSS Shadow Copy for Set Path.
-(gwmi -list win32_shadowcopy).Create('F:\','ClientAccessible') | Out-File "F:\richardscripts\logfiles\Backup Success vssdebug $lastvss.txt" -Width 120;
+(gwmi -list win32_shadowcopy).Create('F:\','ClientAccessible') | Out-File "Full path here with $lastvss.txt" -Width 120;
 # $lastvss Gets date of last shadow copy and outputs blank text file with last shadow copy dd/tt as file name.
 $lastvss = Get-Date(Get-CimInstance -ClassName Win32_shadowcopy | Sort-Object InstallDate | Select-Object -Last 1 -ExpandProperty 'InstallDate') -Format "dd-MM-yyyy hh-mm-ss"
-Out-File "F:\richardscripts\logfiles\Backup Success $lastvss.txt"
-$renamepath = "F:\richardscripts\watchdirectory\opera.txt"
+Out-File "Full Path Here\Backup Success $lastvss.txt"
+$renamepath = "Full Path Here"
 Rename-Item -path $renamepath -newname "Backup Success $lastvss.txt"
 # Writes successful VSS copy event to Applciation EventLog with ID of 1981
 # To find me in Event Viewer, goto Application Logs, Search for
@@ -18,6 +19,6 @@ Write-EventLog -LogName Application -Source OperaVSS -EntryType Information -Eve
 # To find me in Event Viewer, goto Application Logs, Search for;
 # EventId 1981 or Source OperaVSS or Level Information
 Write-EventLog -LogName Application -Source OperaVSS -EntryType Error -EventId 1981 -Message 'OperaVSS Completed, Shadow Copy Failed'
-Out-File "F:\richardscripts\logfiles\Backup Fail $lastvss.txt"
-Out-File "F:\richardscripts\watchdirectory\Backup Fail $lastvss.txt"
+Out-File "Full Path Here"
+Out-File "Full Path here\Backup Fail $lastvss.txt"
 }
